3d89262e6e8bfaa131f0718eb161ccc3d59924f2
chenXie <2501869423@qq.com>
2021-03-16 16:02:13 +0800

    :new: 使用babel配置react

---
+++ chapter09/src/index.js
@@ -0,0 +1,13 @@
+import React, {
+    Component
+}
+from "react"; 
+import ReactDom from "react-dom";
+
+class App extends Component {
+    render() {
+        return <div>Hello Word!</div>
+    }
+}
+
+ReactDom.render(<App />,document.getElementById("root"));
