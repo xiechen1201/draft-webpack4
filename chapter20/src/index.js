7aeee2aea648734d0f4b0ff5973f60343437f663
chenXie <2501869423@qq.com>
2021-03-24 11:51:59 +0800

    :new: 配置PWD

---
+++ chapter20/src/index.js
@@ -0,0 +1,12 @@
+console.log("Hello Word!");
+
+// 如果浏览器支持 serviceWorker
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker.register("/service-worker.js").then(registration => {
+            console.log("service-worker registed")
+        }).catch(error => {
+            console.log("service-worker register error")
+        })
+    })
+}
