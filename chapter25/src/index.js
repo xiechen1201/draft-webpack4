f0565116dc1eef4346477f8d7ab9131483805ac6
chenXie <2501869423@qq.com>
2021-03-30 10:33:00 +0800

    :new: 增加多页面入库配置

---
+++ chapter25/src/index.js
@@ -0,0 +1,16 @@
+import React, {
+    Component
+} from "react";
+import ReactDom from "react-dom";
+
+class App extends Component{
+    Super(){}
+    render(){
+        return(
+            <div>
+                <div>this is index</div>
+            </div>
+        )
+    }
+}
+ReactDom.render(<App />,document.getElementById("root"));
